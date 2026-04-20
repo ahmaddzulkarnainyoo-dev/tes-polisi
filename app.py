3ee45d9e89d7efbf27cd93b7470342b17e458710
ahmaddzulkarnainyoo-dev <ahmaddzulkarnainyoo@gmail.com>
2026-04-20 22:30:16 +0700

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.sidebar.write("🚀 **Project Development**")
 st.sidebar.markdown(
     """
     <a href="https://www.instagram.com/growing_together369?igsh=ZXV1eXpreXkweGh5" target="_blank" style="text-decoration: none; color: #E1306C; font-weight: bold;">
-        Made by @username_temen_lo
+        Made by @growing_together369
     </a>
     """, 
     unsafe_allow_html=True
