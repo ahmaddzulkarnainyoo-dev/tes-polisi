c5ae9a9878fe5b825616026aaacd057fe1a5130c
ahmaddzulkarnainyoo-dev <ahmaddzulkarnainyoo@gmail.com>
2026-04-20 22:05:07 +0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -2,11 +2,20 @@ import streamlit as st
 from groq import Groq
 import json
 import re
+import random
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
-def generate_soal_ai(kategori):
-    prompt = f"Buat 1 soal psikotes Polri {kategori}. JSON format: {{\"pertanyaan\": \"...\", \"opsi\": [\"A\", \"B\", \"C\", \"D\"], \"jawaban\": \"...\"}}. Berikan JSON saja."
+def generate_soal_ai():
+    # List kategori biar soalnya acak (Random)
+    kategori_list = ["Kecerdasan", "Kepribadian", "Wawasan Kebangsaan"]
+    kat = random.choice(kategori_list)
+    
+    prompt = f"""
+    Buat 1 soal psikotes Polri kategori {kat}. 
+    Format JSON: {{"pertanyaan": "...", "opsi": ["A", "B", "C", "D"], "jawaban": "..."}}.
+    Berikan HANYA JSON.
+    """
     
     completion = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
